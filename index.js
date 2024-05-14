6815ab373acbd066ecf2eda02c127ad58b729373
DAlexander5279 <104796691+DAlexander5279@users.noreply.github.com>
2024-05-14 16:18:51 -0400

    blog post request handler

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import express from "express";
 const app = express();
 const port = 3000;
 var posts = [];
+var currId = 0;
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
@@ -11,6 +14,20 @@ app.get("/", (req, res) => {
 
 app.get("/create", (req, res) => {
   res.render("create.ejs");
+});
+
+app.post("/create", (req, res) => {
+  let dte = new Date().toISOString().split("T")[0];
+  let pst = {
+    id: currId,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    date: dte,
+  };
+  currId += 1;
+  posts.push(pst);
+  res.render("index.ejs", { posts: posts });
 });
 
 app.listen(port, () => {
