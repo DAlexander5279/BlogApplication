d9c0ebc51ca57ae1d005d4b7f915369f1e966757
DAlexander5279 <104796691+DAlexander5279@users.noreply.github.com>
2024-05-14 16:03:01 -0400

    Blog form

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ import express from "express";
 
 const app = express();
 const port = 3000;
-
+var posts = [];
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+app.get("/create", (req, res) => {
+  res.render("create.ejs");
 });
 
 app.listen(port, () => {
